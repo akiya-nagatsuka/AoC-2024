6907169df7909aeb1faab39dae6cb48c7d5b6c04
Dmitry Stakhov <dmitry.stakhov@paypay-corp.co.jp>
2024-12-02 02:12:59 +0900

    Add Day01 code

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,5 +1,5 @@
-import java.math.BigInteger
-import java.security.MessageDigest
+//import java.math.BigInteger
+//import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readText
 
@@ -8,12 +8,12 @@ import kotlin.io.path.readText
  */
 fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
 
-/**
- * Converts string to md5 hash.
- */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+///**
+// * Converts string to md5 hash.
+// */
+//fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+//    .toString(16)
+//    .padStart(32, '0')
 
 /**
  * The cleaner shorthand for printing output.
