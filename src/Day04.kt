f4af2e7663cf7fcbac5bf0e1de6fcde0dafdd572
Dmitry Stakhov <dmitry.stakhov@paypay-corp.co.jp>
2024-12-04 19:42:16 +0900

    Refactor Day04 code (part1)

--- src/Day04.kt
+++ src/Day04.kt
@@ -10,29 +10,31 @@ fun main() {
     val additionalWidth = horizontalLines.size + verticalLines.size - 1
 
     for (i in 0..<additionalWidth) {
-        var diagonal = mutableListOf<Char>()
-        for (columnIndex in 0..i) {
-            val rowIndex = i - columnIndex
-            val row = lines.getOrNull(rowIndex)
-            if (row == null) continue
-            val symbol = row.getOrNull(columnIndex)
-            if (symbol == null) continue
-            diagonal.add(symbol)
-        }
-        diagonalLines += diagonal
-    }
+        var diagonal1 = mutableListOf<Char>()
+        var diagonal2 = mutableListOf<Char>()
 
-    for (i in 0..<additionalWidth) {
-        var diagonal = mutableListOf<Char>()
         for (columnIndex in 0..i) {
-            val rowIndex = lines.size - 1 - i + columnIndex
-            val row = lines.getOrNull(rowIndex)
-            if (row == null) continue
-            val symbol = row.getOrNull(columnIndex)
-            if (symbol == null) continue
-            diagonal.add(symbol)
+            val rowIndex1 = i - columnIndex
+            val row1 = lines.getOrNull(rowIndex1)
+            if (row1 != null) {
+                val symbol = row1.getOrNull(columnIndex)
+                if (symbol != null) {
+                    diagonal1.add(symbol)
+                }
+            }
+
+            val rowIndex2 = lines.size - 1 - i + columnIndex
+            val row2 = lines.getOrNull(rowIndex2)
+            if (row2 != null) {
+                val symbol = row2.getOrNull(columnIndex)
+                if (symbol != null) {
+                    diagonal2.add(symbol)
+                }
+            }
         }
-        diagonalLines += diagonal
+
+        diagonalLines += diagonal1
+        diagonalLines += diagonal2
     }
 
     val diagonals = diagonalLines.map { it.windowed(4) }.flatten()
